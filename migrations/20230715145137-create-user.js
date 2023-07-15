820f5ad3c0e818438fc04970f26b3714cfe64799
= <=>
2023-07-15 17:08:07 +0200

    setup migration for users model

---
+++ migrations/20230715145137-create-user.js
@@ -0,0 +1,37 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  // eslint-disable-next-line
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable('users');
+  },
+};
